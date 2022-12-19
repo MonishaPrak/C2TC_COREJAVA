966904c4d987d7e4b4afdba3d25612450d963493
Lenovo <Lenovo@DESKTOP-9GHPQH8>
2022-12-19 04:29:27 -0800

    first commit

---
+++ MoneyMoneyBank/src/framework/BankFactory.java
@@ -0,0 +1,7 @@
+package framework;
+
+public class BankFactory {
+	public abstract  SavingAcc getNewSavingAccount(int accNo,String accNm,float accBal,boolean isSalary);
+	public abstract CurrentAcc getNewCurrentAccount(int accNo,String accNm,float accBal);
+
+}
